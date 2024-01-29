a193702ac3c39b7973efdf58386a899eafdfe7b7
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2024-01-29 17:14:54 +0100

    fix fz for desktop

--- src/components/ui/SectionTitle/SectionTitle.tsx
+++ src/components/ui/SectionTitle/SectionTitle.tsx
@@ -7,13 +7,13 @@ export const SectionTitle: React.FC<SectionTitleProps> = ({
 }) => {
   return hero ? (
     <h1
-      className={`section-heading md:text-[44px] md:tracking-[1.76px] ${className}`}
+      className={`section-heading md:text-[44px] md:tracking-[1.76px] xl:text-[60px] xl:tracking-[2.4px] ${className}`}
     >
       {text}
     </h1>
   ) : (
     <h2
-      className={`section-heading md:text-[40px] md:tracking-[1.6px] ${className}`}
+      className={`section-heading md:text-[40px] md:tracking-[1.6px] xl:text-[60px] xl:tracking-[2.4px] ${className}`}
     >
       {text}
     </h2>
