1d75824f8300f9774c190b8413e74f59ebd535b8
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2024-01-29 16:26:38 +0100

    create SectionTitle.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+// import { SectionTitleExamples } from "@/components/ui/SectionTitle/SectionTitleExamples";
+
 export default async function Home() {
   return (
     <>
       <h1>Home Page</h1>
+
+      {/* <SectionTitleExamples /> */}
     </>
   );
 }
