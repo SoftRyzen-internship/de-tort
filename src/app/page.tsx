82b34a0ce55f960b48728477d08e97f1971217cb
Sergio Nezhigay <abc1971h@gmail.com>
2024-01-29 18:59:15 +0200

    remove files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,11 @@
-import LinkButton from "@/components/ui/LinkButton";
-import Image from "next/image";
+// import { SectionTitleExamples } from "@/components/ui/SectionTitle/SectionTitleExamples";
 
 export default async function Home() {
   return (
-    <div className="bg-green-100 p-4">
-      <h1 className="bg-color-bg-white mb-4 p-4">Home Page</h1>
-      <div className="bg-red-100 w-[300px] mb-4 p-4">
-        <LinkButton label="Каталог тортів" href="/single-label" />
-      </div>
-      <div className="bg-blue-100 w-[500px] mb-4 p-4">
-        <LinkButton
-          label="Primary Label"
-          secondaryLabel="Secondary Label"
-          href="/dual-label"
-          styles="mx-auto"
-        />
-        <Image
-          src="/public/images/tort-sliced-test.webp"
-          // src="/public/images/tort-sliced.png"
-          alt="sdg"
-          width={100}
-          height={100}
-        />
-      </div>
-    </div>
+    <>
+      <h1>Home Page</h1>
+
+      {/* <SectionTitleExamples /> */}
+    </>
   );
 }
--- src/components/ui/LinkButton/types.ts
+++
@@ -1,8 +0,0 @@
-import { ClassValue } from "clsx";
-import { LinkProps } from "next/link";
-
-export type LinkButtonProps = LinkProps & {
-  label: string;
-  secondaryLabel?: string;
-  styles?: ClassValue;
-};
