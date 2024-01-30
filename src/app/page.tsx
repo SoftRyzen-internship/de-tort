55ac6c69ed5ee76814abacb291d2054483fcb36d
Sergio Nezhigay <abc1971h@gmail.com>
2024-01-30 10:30:15 +0200

    hero start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 // import { SectionTitleExamples } from "@/components/ui/SectionTitle/SectionTitleExamples";
 
+import { Hero } from "@/sections/Hero";
+
 export default async function Home() {
   return (
     <>
-      <h1>Home Page</h1>
-
+      {/* <h1>Home Page</h1> */}
+      <Hero />
       {/* <SectionTitleExamples /> */}
     </>
   );
--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -1,3 +1,36 @@
+import { SectionTitle } from "@/components/ui/SectionTitle";
+import Image from "next/image";
+import Link from "next/link";
+
 export const Hero = () => {
-  return <section></section>;
+  return (
+    <section className="bg-color-bg-hero py-[90px]">
+      <div className="pl-5">
+        <div className="relative mx-auto size-[81vw]">
+          <Image
+            src="/images/tort-whole-@x2.webp"
+            alt="whole tort"
+            fill
+            className="object-cover"
+            sizes="100%"
+          />
+        </div>
+      </div>
+
+      <div className="container">
+        <SectionTitle
+          hero
+          text="Онлайн-кондитерська"
+          className="max-w-[694px]"
+        />
+        <p>
+          Спеціалізується на виробництві тортиків та інших смаколиків по
+          індивідуальним замовленням!
+        </p>
+        <Link href="/" className="bg-red-100 p-4 w-fit block">
+          Каталог тортів
+        </Link>
+      </div>
+    </section>
+  );
 };
