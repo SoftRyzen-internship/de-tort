709e14a3d0cd75e7f8428ed3923504802baff68e
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2024-01-30 19:44:59 +0100

    implement header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
-import { ContactLinks } from "@/components/ui/ContactLinks";
-import { Logo } from "@/components/ui/Logo";
-import Example from "@/components/Example";
-
+// import { ContactLinks } from "@/components/ui/ContactLinks";
+// import { Logo } from "@/components/ui/Logo";
+// import Example from "@/components/Example";
 // import { SectionTitleExamples } from "@/components/ui/SectionTitle/SectionTitleExamples";
 
 export default async function Home() {
   return (
     <>
-      <Logo />
-      <h1>Home Page</h1>
-      <ContactLinks variant="footer" />
+      <section className="pt-[88px] md:pt-[160px] xl:pt-[204px] bg-color-bg-hero text-center">
+        <h1>Home Page - hero section</h1>
+      </section>
 
-      <Example />
+      {/* <ContactLinks variant="footer" /> */}
+      {/* <Example /> */}
       {/* <SectionTitleExamples /> */}
     </>
   );
