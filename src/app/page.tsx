a9f3c92c31c1f35a537408e4e1128fb2691f9a78
Lika <114234069+Basilika-draconum@users.noreply.github.com>
2024-01-30 11:28:36 +0200

    add styles for text, fix json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 // import { SectionTitleExamples } from "@/components/ui/SectionTitle/SectionTitleExamples";
 
+import { Accordion } from "@/components/base/Accordion";
+
 export default async function Home() {
   return (
     <>
       <h1>Home Page</h1>
 
       {/* <SectionTitleExamples /> */}
+      <div className="container">
+        <Accordion />
+      </div>
     </>
   );
 }
--- src/components/ui/AccordionItem/AccordionItem.tsx
+++ src/components/ui/AccordionItem/AccordionItem.tsx
@@ -4,8 +4,12 @@ export const AccordionItem: React.FC<AccordionItemType> = ({ item }) => {
   const { question, answer } = item;
   return (
     <details className="">
-      <summary className="">{question}</summary>
-      <p className="">{answer}</p>
+      <summary className="font-semibold text-sm leading-[1.4] text-color-text-primary">
+        {question}
+      </summary>
+      <p className="font-fixel text-xl text-color-text-secondary leading-[1.4] font-normal">
+        {answer}
+      </p>
     </details>
   );
 };
