d3e4d21785f006a8eac703bdb05b291234fc10d2
Lika <114234069+Basilika-draconum@users.noreply.github.com>
2024-02-01 11:28:44 +0200

    add styles

--- src/components/base/AchievementCards/AchievementCards.tsx
+++ src/components/base/AchievementCards/AchievementCards.tsx
@@ -5,7 +5,8 @@ import json from "@/data/achievement.json";
 export const AchievementCards: React.FC = () => {
   const { cards } = json;
   return (
-    <ul className="flex-center xl:gap-8 ">
+    //змінити стилі при появі слайдера
+    <ul className="flex-center flex-wrap gap-8 xl:gap-8 ">
       {cards.map(({ id, title, description }) => (
         <AchievementCard
           key={id}
