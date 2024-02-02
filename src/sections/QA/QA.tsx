3ee013cdd4f02323a217a5fdff09ceb486e7bc46
Lika <114234069+Basilika-draconum@users.noreply.github.com>
2024-02-02 13:10:00 +0200

    edit ReadME, delete rules from eslint, fix global class .section and update Qa section

--- src/sections/QA/QA.tsx
+++ src/sections/QA/QA.tsx
@@ -5,11 +5,11 @@ import json from "@/data/qa.json";
 
 export const QA: React.FC = () => {
   return (
-    <section className="section bg-color-bg-primary">
-      <div className="container">
+    <section className="section bg-color-bg-primary pb-[60px] mdOnly:pb-20 xl:pb-[120px]">
+      <div className="container flex flex-col xl:flex-row xl:gap-8">
         <SectionTitle
           text={json.titleSection}
-          className="flex flex-col mb-6 md:mb-10 xl:flex-row xl:mb-0 xl:gap-8"
+          className="mb-1 md:mb-5 xl:mb-0 mdOnly:w-[286px]"
         />
         <Accordion />
       </div>
