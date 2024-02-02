ca930ab77e6306dbbbadb26ddefc49780dbe2304
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2024-02-02 14:07:48 +0100

    create cakes assortment section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import { Hero } from "@/sections/Hero";
 import { Contacts } from "@/sections/Contacts";
 import { About } from "@/sections/About";
 import { Sweets } from "@/sections/Sweets";
+import { CakesAssortment } from "@/sections/CakesAssortment";
 
 export default async function Home() {
   return (
     <>
       <Hero />
+      <CakesAssortment />
       <Sweets />
       <About />
       <Contacts />
---
+++ src/sections/CakesAssortment/CakesAssortment.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+
+import { LinkButton } from "@/components/ui/LinkButton";
+import { SectionTitle } from "@/components/ui/SectionTitle";
+
+import data from "@/data/cakes-assortment.json";
+
+export const CakesAssortment: React.FC = () => {
+  const { heading, links, image } = data;
+  return (
+    <section className="section xl:pt-[120px] bg-color-bg-primary">
+      <div className="container xl:flex items-center justify-between">
+        <Image
+          src={image.path}
+          alt={image.alt}
+          width={1186}
+          height={1040}
+          className="hidden xl:block w-[592] h-[520]"
+        />
+
+        <div className="flex flex-col gap-6 md:gap-10 xl:w-[592px]">
+          <SectionTitle
+            text={heading}
+            className="max-w-[400px] xl:max-w-[575px]"
+          />
+
+          <ul className="flex flex-col gap-3 md:gap-4">
+            {links.map(({ slug, label, caption }) => (
+              <li key={slug}>
+                <LinkButton
+                  label={label}
+                  secondaryLabel={caption}
+                  href={`/${slug}`}
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
