463675450f90c903aeb609f9ee3afa6a2ece98fb
Sergio Nezhigay <abc1971h@gmail.com>
2024-02-04 12:38:14 +0200

    change to svg

--- src/components/ui/Logo/Logo.tsx
+++ src/components/ui/Logo/Logo.tsx
@@ -1,13 +1,13 @@
-import Image from "next/image";
-
 import { cn } from "@/utils/helpers";
+
+import LogoIcon from "~/icons/logo.svg";
 
 import content from "@/data/common.json";
 
 import { TLogo } from "./types";
 
 export const Logo: React.FC<TLogo> = ({ className = "" }) => {
-  const { companyName, logo } = content;
+  const { companyName } = content;
 
   return (
     <a
@@ -18,7 +18,7 @@ export const Logo: React.FC<TLogo> = ({ className = "" }) => {
       )}
       aria-label={companyName}
     >
-      <Image priority src={logo.path} alt={logo.alt} width={58} height={64} />
+      <LogoIcon className="h-[64px] w-[58px]" />
     </a>
   );
 };
