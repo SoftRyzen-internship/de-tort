ba054728f49f14114f22e90b09547bef45a6caf2
Sergio Nezhigay <abc1971h@gmail.com>
2024-02-04 11:25:16 +0200

    add space

--- src/sections/Sweets/Sweets.tsx
+++ src/sections/Sweets/Sweets.tsx
@@ -10,6 +10,7 @@ export const Sweets = () => {
     <section className="section bg-color-bg-primary">
       <div className="container">
         <SectionTitle text={title} className="sr-only" />
+
         <ul className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 md:gap-8">
           {sweetList.map(
             ({ name, nameColor, bgSrc, bgAlt, targetHref }, index) => (
