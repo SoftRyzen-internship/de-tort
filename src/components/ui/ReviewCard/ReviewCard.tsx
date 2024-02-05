194d5853ad9a119d911ad7518baee7ce5e16f37e
Sergio Nezhigay <abc1971h@gmail.com>
2024-02-05 16:31:22 +0200

    width change

--- src/components/ui/ReviewCard/ReviewCard.tsx
+++ src/components/ui/ReviewCard/ReviewCard.tsx
@@ -15,7 +15,7 @@ export const ReviewCard: React.FC<ReviewCardProps> = ({
   const normalizedText = text.slice(0, MAX_SYMBOLS);
 
   return (
-    <div className="bg-white md:w-[336px] xl:w-[384px] shadow-3xl-a font-fixel rounded-3xl border border-color-accent-primary w-full text-base leading-normal">
+    <div className="bg-white shadow-3xl-a font-fixel rounded-3xl border border-color-accent-primary w-full text-base leading-normal">
       <h3 className="py-6 flex-center gap-2 border-b border-color-accent-primary text-[24px]">
         <span className="text-color-option-seven ">@</span>
         <span>{author}</span>
@@ -24,13 +24,13 @@ export const ReviewCard: React.FC<ReviewCardProps> = ({
         <p className="text-center text-color-option-eight mb-2">{date}</p>
         <div className="text-white relative bg-gradient-to-r whitespace-pre-line from-color-option-nine py-6 px-4 xl:px-6 to-color-option-ten rounded-3xl ">
           <p>{normalizedText}</p>
-          <div className="w-[38px] md:w-10 text-center h-8 border-white border-2 rounded-full absolute  bottom-0  bg-color-bg-grey right-6 translate-y-1/2 flex-center text-[16px]">
+          <div className="w-[38px] md:w-10 text-center h-8 border-white border-2 rounded-full absolute  bottom-0 bg-color-bg-grey right-6 translate-y-1/2 flex-center text-[16px]">
             <Image
               src={imageHeart.path}
               alt={imageHeart.alt}
-              width={0}
-              height={0}
-              className="object-cover w-auto"
+              width={512}
+              height={544}
+              className="object-cover w-[16px] h-auto"
               sizes="16px"
             />
           </div>
