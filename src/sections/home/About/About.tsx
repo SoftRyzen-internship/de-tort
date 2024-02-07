cbdbcc70c13fe72fa93d1eef9d9acff44564018e
Sergio Nezhigay <abc1971h@gmail.com>
2024-02-07 18:04:19 +0200

    change about structure

--- src/sections/home/About/About.tsx
+++ src/sections/home/About/About.tsx
@@ -8,24 +8,26 @@ import about from "@/data/about.json";
 export const About: React.FC = () => {
   const { values, features, title, description } = about;
 
+  const subHeaderStyle =
+    "font-unbounded mb-3 xl:mb-4 font-semibold text-[20px] xl:text-[24px] leading-normal";
+
   return (
     <section className="section bg-color-bg-primary">
       <div className="container text-base md:text-xl text-color-text-secondary whitespace-pre-line leading-normal md:flex md:gap-[34px] xl:gap-[240px]">
-        <div>
+        <div className="smOnly:mb-10">
           <SectionTitle text={title} className="mb-4" />
 
-          <p className="mb-6 font-fixel md:mb-8 xl:mb-12">{description}</p>
+          <p className="font-fixel mb-6 md:mb-8 xl:mb-12">{description}</p>
 
-          <div className="smOnly:mb-10 xl:max-w-[416px] space-y-6 md:space-y-8 xl:space-y-10">
-            {values.map(({ title, text }, index) => (
-              <div key={index}>
-                <h3 className="font-unbounded mb-3 xl:mb-4 font-semibold text-[20px] xl:text-[24px] leading-normal">
-                  {title}
-                </h3>
-                <p className="font-fixel">{text}</p>
-              </div>
-            ))}
-          </div>
+          <h3 className={subHeaderStyle}>{values[0].title}</h3>
+
+          <p className="font-fixel mb-6 md:mb-8 xl:mb-10 xl:max-w-[416px]">
+            {values[0].text}
+          </p>
+
+          <h3 className={subHeaderStyle}>{values[1].title}</h3>
+
+          <p className="font-fixel">{values[1].text}</p>
         </div>
         <ul className="flex flex-col gap-4">
           {features.map(({ text, image }, index) => {
