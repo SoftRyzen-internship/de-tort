2c40a2f412d9b53171d7930073ef71cde72f9225
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2024-02-08 12:58:36 +0100

    init cake page

---
+++ src/app/[cake]/page.tsx
@@ -0,0 +1,31 @@
+import json from "@/data/cakes-assortment.json";
+
+export const dynamicParams = false;
+export const dynamic = "error";
+export const revalidate = false;
+
+export async function generateStaticParams() {
+  const staticParams = json.links.map(({ slug }) => {
+    return { cake: slug };
+  });
+
+  return staticParams;
+}
+
+export default async function CakePage({
+  params: { cake },
+}: {
+  params: { cake: string };
+}) {
+  console.log("slug: ", cake);
+
+  return (
+    <>
+      <section className="py-[240px] bg-color-bg-primary">
+        <div className="container">
+          <p>Current page: {cake}</p>
+        </div>
+      </section>
+    </>
+  );
+}
