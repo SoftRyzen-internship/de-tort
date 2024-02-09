5f002ca8a4fab80fd584b9fe48931332fa651bea
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2024-02-09 12:29:39 +0100

    add some test styles to modal

--- src/components/ui/Modal/Modal.tsx
+++ src/components/ui/Modal/Modal.tsx
@@ -19,30 +19,38 @@ export const Modal: React.FC<ModalProps> = ({
   return (
     <Transition show={isOpen} as={Fragment}>
       <Dialog onClose={onClose} className="relative z-50">
-        <div className="fixed inset-0 bg-color-bg-backdrop w-screen">
-          <div
-            className={`flex items-start justify-center text-center container min-h-full ${classNameWrap}`}
-          >
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
+        <div
+          // className="fixed inset-0 bg-color-bg-backdrop w-screen"
+          className="fixed inset-0 bg-color-bg-backdrop"
+        >
+          <div className="fixed inset-0 w-screen overflow-y-auto">
+            <div
+              // className={`flex items-start justify-center text-center container min-h-full ${classNameWrap}`}
+              className={`min-h-full py-6 flex items-center justify-center text-center  ${classNameWrap}`}
             >
-              <Dialog.Panel
-                className={`fixed max-w-[480px] h-full rounded-3xl bg-color-bg-white border-2 border-color-accent-primary p-4 text-left transition-all mt-[120px] md:py-[60px] md:px-9 md:max-w-[704px] xl:max-w-[1216px] xl:py-10 xl:px-[104px] ${classNameModal}`}
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
               >
-                <CloseButton
-                  ariaLabel={close}
-                  onClick={onClose}
-                  className="absolute top-4 right-4 md:top-[60px] md:right-9 xl:top-9"
-                />
-                {children}
-              </Dialog.Panel>
-            </Transition.Child>
+                <Dialog.Panel
+                  // className={`fixed max-w-[480px] h-full rounded-3xl bg-color-bg-white border-2 border-color-accent-primary p-4 text-left transition-all mt-[120px] md:py-[60px] md:px-9 md:max-w-[704px] xl:max-w-[1216px] xl:py-10 xl:px-[104px] ${classNameModal}`}
+                  className={`relative max-w-[480px] h-[90vh] rounded-3xl bg-color-bg-white border-2 border-color-accent-primary p-4 text-left md:py-[60px] md:px-9 md:max-w-[704px] xl:max-w-[1216px] xl:py-10 xl:px-[104px] ${classNameModal}`}
+                  // className={`relative max-w-[480px] rounded-3xl bg-color-bg-white border-2 border-color-accent-primary p-4 text-left md:py-[60px] md:px-9 md:max-w-[704px] xl:max-w-[1216px] xl:py-10 xl:px-[104px] ${classNameModal}`}
+                >
+                  <CloseButton
+                    ariaLabel={close}
+                    onClick={onClose}
+                    className="absolute top-4 right-4 md:top-[60px] md:right-9 xl:top-9"
+                  />
+                  {children}
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
           </div>
         </div>
       </Dialog>
--- src/sections/cakes/ToppingsModal/ToppingsModal.tsx
+++ src/sections/cakes/ToppingsModal/ToppingsModal.tsx
@@ -4,34 +4,48 @@ import { ToppingModalProps } from "./types";
 
 export const ToppingsModal: React.FC<ToppingModalProps> = ({ onClose }) => {
   return (
-    <div className="flex flex-col h-full">
-      <SectionTitle text="Оберіть начинку" className="mb-8 smOnly:w-[186px]" />
-      <ul className="overflow-y-auto flex justify-center flex-col gap-[17px] md:gap-3 md:flex-row md:flex-wrap">
-        <li
-          className=" w-[310px] h-[404px] bg-color-bg-hero card-cakes"
-          onClick={onClose}
+    <div
+      // className="flex flex-col h-full"
+      className="flex flex-col h-full gap-8"
+      // className="h-full"
+    >
+      <SectionTitle
+        text="Оберіть начинку"
+        // className="mb-8 smOnly:w-[186px]"
+        className="smOnly:w-[186px]"
+      />
+
+      <div className="grow overflow-y-auto">
+        <ul
+          // className="overflow-y-auto flex justify-center flex-col gap-[17px] md:gap-3 md:flex-row md:flex-wrap"
+          className="flex justify-center flex-col gap-[17px] md:gap-3 md:flex-row md:flex-wrap"
         >
-          b
-        </li>
-        <li
-          className=" w-[310px] h-[404px] bg-color-bg-hero card-cakes"
-          onClick={onClose}
-        >
-          b
-        </li>
-        <li
-          className=" w-[310px] h-[404px] bg-color-bg-hero card-cakes"
-          onClick={onClose}
-        >
-          b
-        </li>
-        <li
-          className=" w-[310px] h-[404px] bg-color-bg-hero card-cakes"
-          onClick={onClose}
-        >
-          b
-        </li>
-      </ul>
+          <li
+            className="shrink-0 w-[310px] h-[404px] bg-color-bg-hero card-cakes"
+            onClick={onClose}
+          >
+            a
+          </li>
+          <li
+            className="shrink-0 w-[310px] h-[404px] bg-color-bg-hero card-cakes"
+            onClick={onClose}
+          >
+            b
+          </li>
+          <li
+            className="shrink-0 w-[310px] h-[404px] bg-color-bg-hero card-cakes"
+            onClick={onClose}
+          >
+            c
+          </li>
+          <li
+            className="shrink-0 w-[310px] h-[404px] bg-color-bg-hero card-cakes"
+            onClick={onClose}
+          >
+            d
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
