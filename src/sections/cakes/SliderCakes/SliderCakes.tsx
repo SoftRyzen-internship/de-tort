3fb41d11f7c664bff5743afbdcde2287b430dd29
TrueMolni <truemolni@gmail.com>
2024-02-09 10:43:38 +0200

    add slider changes, add markup

--- src/sections/cakes/SliderCakes/SliderCakes.tsx
+++ src/sections/cakes/SliderCakes/SliderCakes.tsx
@@ -6,6 +6,11 @@ import { SectionTitle } from "@/components/ui/SectionTitle";
 
 import { ToppingCard } from "@/components/ui/ToppingCard";
 
+import Dollar from "~/icons/dollar-circle.svg";
+import Slice from "~/icons/cake-slice.svg";
+import Clock from "~/icons/clock.svg";
+import Star from "~/icons/star.svg";
+
 import data from "@/data/topppings.json";
 const { toppings } = data;
 
@@ -16,12 +21,48 @@ const slides = toppings?.map(({ label, src, alt, id }, idx) => (
 export const SliderCakes: FC = () => {
   return (
     <section className="section bg-color-bg-accent py-[60px] md:py-[80px] xl:py-[120px]">
-      <div className="container">
-        <div className="relative px-6">
+      <div className="container xl:flex xl:gap-[60px]">
+        <div className="relative xl:w-[592px]">
           <Slider slides={slides} section="cakes" />
           <SliderNav section="cakes" />
         </div>
-        <SectionTitle text="Бенто-торт" />
+        <div>
+          <SectionTitle text="Бенто-торт" />
+          <div className=" font-fixel md:flex">
+            <div className="flex">
+              <Slice className="w-6" />
+              <div className="">
+                <p className="font-bold text-base leading-normal text-color-text-secondary">
+                  Розрахунок:
+                </p>
+
+                <p> на 2-х людей</p>
+              </div>
+            </div>
+            <div className="flex">
+              <Clock className="w-6" />
+              <div className="">
+                <p className="font-bold">Термін виготовлення:</p>
+                <p>1 робочий день </p>
+              </div>
+            </div>
+          </div>
+          <p>
+            Маленький торт 400-500 грамів, на будь-яку подію або просто так,
+            індивідуальний дизайн та соковита начинка, а ще в комплекті свічка і
+            ложечка!
+          </p>
+          <div>
+            <Star className="w-6" />
+            <p>Декор:</p>
+            <p>напис, малюнок</p>
+          </div>
+          <div>
+            <Dollar className="w-6" />
+            <p>Ціна:</p>
+            <p>600 грн - з малюнком, 700 грн - з написом</p>
+          </div>
+        </div>
       </div>
     </section>
   );
