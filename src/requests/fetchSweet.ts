59a2f238f772289847bd9ad1561cf6365f265779
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2024-02-13 20:51:41 +0100

    add sweet-item request

---
+++ src/requests/fetchSweet.ts
@@ -0,0 +1,41 @@
+import { request } from "graphql-request";
+
+import { getSweet } from "@/requests/queries/getSweet";
+
+import { SweetsSlug, SweetDataType, SweetsItemType } from "@/types";
+
+export const fetchSweet = async (
+  sweet: SweetsSlug,
+): Promise<SweetsItemType[]> => {
+  try {
+    const res: SweetDataType = await request(
+      process.env.API_BASE_URL as string,
+      getSweet,
+      {
+        sweet: sweet,
+      },
+    );
+    // console.log("fetch res: ", res);
+
+    const data = res.sweets.data.map(({ attributes }) => {
+      const sweetImages = attributes.images.map(
+        ({ image_description, photo }) => {
+          return {
+            desc: image_description,
+            url: photo.data.attributes.url,
+          };
+        },
+      );
+
+      return {
+        ...attributes,
+        images: sweetImages,
+      };
+    });
+
+    return data;
+  } catch (error) {
+    console.log("fetch sweet error: ", error);
+    return [];
+  }
+};
