ce67cd4da21e687256fef7aff2599e48528baa5f
Lika <114234069+Basilika-draconum@users.noreply.github.com>
2024-02-16 15:00:08 +0200

    set height on auto in card

--- src/components/ui/ToppingModalCard/ToppingModalCard.tsx
+++ src/components/ui/ToppingModalCard/ToppingModalCard.tsx
@@ -30,7 +30,7 @@ export const ToppingModalCard: React.FC<ToppingModalCardProps> = ({
       onClick={onClick}
       tabIndex={0}
       onKeyDown={handleKeyDown}
-      className="modal-card shrink-0 w-[288px] h-[411px] md:w-auto md:h-[404px] xl:h-[386px] px-3 md:px-4 pb-[15px] md:pb-[30px] xl:pb-[24px] text-[16px] font-unbounded border rounded-3xl leading-normal group border-topping-card-even hover:border-color-accent-primary focus-visible:border-color-accent-primary bg-color-bg-white hover:shadow-3xl focus:shadow-3xl transition-all cursor-pointer"
+      className="modal-card shrink-0 w-[288px] h-auto md:w-auto px-3 md:px-4 pb-[15px] md:pb-[30px] xl:pb-[24px] text-[16px] font-unbounded border rounded-3xl leading-normal group border-topping-card-even hover:border-color-accent-primary focus-visible:border-color-accent-primary bg-color-bg-white hover:shadow-3xl focus:shadow-3xl transition-all cursor-pointer"
     >
       <div className="relative plate-modal h-[173px] xl:h-[162px] overflow-hidden">
         <Image
