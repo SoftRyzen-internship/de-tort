52c233b8c04e5bd0bfaee79eca6015583fd9fafa
TrueMolni <truemolni@gmail.com>
2024-02-21 14:52:54 +0200

    change margins

--- src/sections/offer/PublicOffer/PublicOffer.tsx
+++ src/sections/offer/PublicOffer/PublicOffer.tsx
@@ -9,10 +9,10 @@ export const PublicOffer: React.FC<OfferProps> = ({ data }) => {
     "prose-h1:text-color-accent-primary prose-strong:font-bold prose-strong:md:tracking-base";
 
   const title =
-    "prose-h1:text-center prose-h1:leading-base prose-h1:text-[28px] prose-h1:md:text-[40px] prose-h1:xl:text-[48px] prose-h1:xl:tracking-normal prose-h1:uppercase";
+    "prose-h1:mb-7 prose-h1:md:mb-9 prose-h1:text-center prose-h1:leading-base prose-h1:text-[28px] prose-h1:md:text-[40px] prose-h1:xl:text-[48px] prose-h1:xl:tracking-normal prose-h1:uppercase";
 
   const subtitle =
-    "prose-h2:text-xl prose-h2:leading-normal prose-h2:text-color-text-secondary prose-h2:font-fixel prose-h2:font-semibold prose-h2:md:text-[24px]";
+    "prose-h2:mb-4 prose-h2:mt-7 prose-h2:md:mt-9 prose-h2:text-xl prose-h2:text-xl prose-h2:leading-normal prose-h2:text-color-text-secondary prose-h2:font-fixel prose-h2:font-semibold prose-h2:md:text-[24px]";
 
   const paragrapgh =
     "prose-p:font-fixel prose-p:text-base prose-p:leading-normal prose-p:text-color-text-secondary prose-p:md:text-xl prose-p:md:leading-normal prose-p:text-justify";
