f51a238414c56af79524d50e4d8d3e63b41b6a22
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2024-02-21 13:27:27 +0100

    test-fix tag 3

--- src/components/ui/AchievementCard/AchievementCard.tsx
+++ src/components/ui/AchievementCard/AchievementCard.tsx
@@ -8,7 +8,7 @@ export const AchievementCard: React.FC<AchievementCardProps> = ({
   return (
     <div className="bg-color-bg-accent p-2 rounded-3xl py-8 px-4 relative md:w-[336px] md:px-6 xl:px-8 xl:w-[384px]">
       <div className="h-[40px] font-fixel absolute top-0 -translate-y-1/2 right-6 border-color-accent-primary border-2 bg-color-bg-white px-2 rounded flex-center">
-        <span className="text-color-accent-primary font-medium text-[24px] h-[24px] leading-none">
+        <span className="text-color-accent-primary font-medium text-[24px] h-[24px] leading-[1]">
           #{id}
         </span>
       </div>
